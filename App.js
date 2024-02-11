4f699234c62dc828ece55dce2bbee0f5980ff20b
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2024-02-11 17:05:36 +0000

    image done

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, Text, View } from "react-native";
+import { ImageBackground, SafeAreaView, Text, View } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+const image = {
+  uri: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+};
 
 export default function App() {
   return (
@@ -34,8 +40,58 @@ export default function App() {
       </View>
       {/**top section end */}
 
+      {/**image section start */}
+      <View className="my-8">
+        <ImageBackground source={image} className="h-32" borderRadius={10}>
+          <View className="p-4">
+            <Text className="text-white">Hi Sam</Text>
+            <Text className="text-white font-semibold text-3xl">
+              Fresh start your journey
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
+      {/**image section start */}
 
-      
+      {/**vehicle section start */}
+      <View className="flex-row items-center gap-2">
+
+
+        {/**View one start */}
+        <View className="flex-row items-center gap-1 px-4 py-2 bg-black rounded-lg ">
+          {/**icon view start */}
+          <View>
+            <AntDesign name="car" size={20} color="white" />
+          </View>
+          {/**icon view end */}
+          {/**text view start */}
+          <View>
+            <Text className="text-white">Cars</Text>
+          </View>
+          {/**text view end */}
+        </View>
+        {/**View one end */}
+
+
+        {/**View tow start */}
+        <View className="flex-row items-center gap-1 px-4 py-2 rounded-lg bg-gray-100">
+          <View>
+ <Ionicons name="bus-outline" size={20}/>
+          </View>
+          <View>
+<Text>
+  Bus
+</Text>
+          </View>
+
+        </View>
+        {/**View two end */}
+        {/**View 3 start */}
+       
+        {/**View 4 end */}
+      </View>
+
+      {/** vehicle section end*/}
     </SafeAreaView>
   );
 }
